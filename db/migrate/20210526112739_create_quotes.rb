f75133f4068ab0e2424b52e47525d2b417440f4f
tahiry-dev <tahirycontact@gmail.com>
2021-05-26 14:32:47 +0300

    delete migration

---
+++ db/migrate/20210526112739_create_quotes.rb
@@ -0,0 +1,12 @@
+class CreateQuotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :quotes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :author
+      t.text :description
+      t.string :ratings
+
+      t.timestamps
+    end
+  end
+end
