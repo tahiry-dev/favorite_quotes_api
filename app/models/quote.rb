743c9a07cb4517d8150573229eb764c60309ba02
tahiry-dev <tahirycontact@gmail.com>
2021-05-27 15:13:24 +0300

    fix stylint

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -13,19 +13,19 @@ class Quote < ApplicationRecord
 
   validates :description, presence: true, length: { minimum: 6, maximum: 50_000 }
 
-  # def set_host
-  #   Rails.env.production? ? 'https://handmades-rails-api-backend.herokuapp.com' : 'http://localhost:3000'
-  # end
+  def set_host
+    Rails.env.production? ? 'https://your-favorite-quotes-api.herokuapp.com' : 'http://localhost:3000'
+  end
 
-  # def image_url
-  #   if image.nil?
+  def image_url
+    if image.nil?
 
-  #     "<img src='http://loremflickr.com/600/800/man' alt='Man'/>"
+      "<img src='http://loremflickr.com/600/800/man' alt='Man'/>"
 
-  #   else
+    else
 
-  #     rails_representation_url(image.variant(resize: '600x800'), host: set_host)
+      rails_representation_url(image.variant(resize: '600x800'), host: set_host)
 
-  #   end
-  # end
+    end
+  end
 end
