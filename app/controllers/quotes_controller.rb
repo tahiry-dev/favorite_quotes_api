dada77ead91012811684beaf5c208ed709c4ef4a
tahiry-dev <tahirycontact@gmail.com>
2021-07-18 07:53:39 +0300

    write serialization function inside application controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,19 +4,18 @@ class QuotesController < ApplicationController
 
   def index
     @quotes = Quote.all.order('created_at DESC')
-
-    render json: serialize_quotes(@quotes)
+      render json: serialize_quotes(@quotes)
   end
 
   def show
-    render json: serialize_quote(@quote)
+      render json: serialize_quote(@quote)
   end
 
   def create
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      render json: serialize_quote(@quote), status: :created, location: @quote
+       render json: serialize_quote(@quote), status: :created, location: @quote
     else
       render json: @quote.errors.full_messages, status: :unprocessable_entity
     end
@@ -32,7 +31,7 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    render json: serialize_quote(@quote)
+     render json: serialize_quote(@quote)
   end
 
   private
@@ -41,22 +40,4 @@ class QuotesController < ApplicationController
     params.permit(:user_id, :author, :description, :image)
   end
 
-  def serialize_quotes(quotes)
-    quotes.map { |quote| serialize_quote(quote) }
-  end
-
-  def serialize_quote(quote)
-    {
-      id: quote.id,
-      author: quote.author,
-      description: quote.description,
-      ratings: quote.ratings,
-      image_url: quote.image_url,
-      created_at: quote.created_at,
-      updated_at: quote.updated_at,
-      user_id: quote.user_id,
-      user_name: quote.user.name,
-      favorited_by: quote.favorited_by
-    }
-  end
 end
