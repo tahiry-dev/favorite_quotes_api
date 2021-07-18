ebc58228f9abc3995064a1fa85feda2caa5c2122
tahiry-dev <tahirycontact@gmail.com>
2021-07-18 08:36:17 +0300

    fix and add rspec test

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:favorites) }
+    it { should have_many(:favorited_by) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:author) }
+    it { is_expected.to validate_presence_of(:description) }
+   
+    it do
+      should validate_length_of(:author).is_at_least(2).is_at_most(40)
+    end
+    it do
+      should validate_length_of(:description).is_at_least(6).is_at_most(50_000)
+    end
+  end
+
+  describe 'scopes' do
+    before :each do
+      @user = FactoryBot.create(:user)
+      @quote = @user.quotes.create!(author: 'Example', description: 'Example desx')
+    end
+
+    it 'can be created by user' do
+      expect(Quote.first).to eq(@quote)
+    end
+
+    it 'can be accessed by the author' do
+      expect(@user.quotes.first).to eq(@quote)
+    end
+  end
 end
